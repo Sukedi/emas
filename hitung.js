b879e6808c4925493c8429ef20dd94d00b1015c8
Sukedi <169124018+Sukedi@users.noreply.github.com>
2025-03-09 13:43:32 +0700

    Update hitung.js

--- hitung.js
+++ hitung.js
@@ -1,8 +1,5 @@
 function showHasil(){ hasil.style.left = "50%"; hasil.style.opacity = "1"; }
 async function hitung(){
-    // disini parameter warna...
-    let brt1 = document.getElementById("w1").value;
-    let brt2 = document.getElementById("w2").value;
     if (brt1 * brt2 == 0){
         Swal.fire({
           title: "Error",
@@ -12,10 +9,16 @@ async function hitung(){
         return;
     }
     tunggu();
-    hsl=brt1/brt2;
+    var warnaDipilih = document.querySelector('input[name="warna"]:checked');
+    NC = warnaDipilih.value;
+    let brt1 = document.getElementById("w1").value;
+    let brt2 = document.getElementById("w2").value;
+    SG = brt1/(brt1-brt2);
+ //   hsl = 
     document.getElementById("hasil").innerHTML= "Kadar : "+hsl.toFixed(2);
     document.getElementById("w1").value="";
     document.getElementById("w2").value="";
+    document.getElementById("c0").checked = true;
     document.getElementById("c1").checked = false;
     document.getElementById("c2").checked = false;
     document.getElementById("c3").checked = false;
