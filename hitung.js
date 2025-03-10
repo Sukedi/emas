12ca38f18977dc65e40b9404b14e37547ce9d9d9
Sukedi <169124018+Sukedi@users.noreply.github.com>
2025-03-10 11:39:33 +0700

    Update hitung.js

--- hitung.js
+++ hitung.js
@@ -11,7 +11,7 @@ async function hitung(){
         return;
     }
     tunggu();
-    ratio = decript("101","Ò.G#æFEýßÔ=ûãÛöF0îÑÞ");nxy=(brt1-brt1);
+    ratio = decript("101","Ò.G#æFEýßÔ=ûãÛöF0îÑÞ");
     var warnaDipilih = document.querySelector('input[name="warna"]:checked');
     NC = warnaDipilih.value;
     SG = brt1/(brt1-brt2);
@@ -20,7 +20,7 @@ async function hitung(){
     brt0 = document.getElementById("emas").innerHTML;
     brtx = ratio + " - " + brt0;
     alert(brtx);
-    if (ratio <> brt0){ hsl=nxy; }
+//    if (ratio != brt0){ hsl=0; }
     document.getElementById("hasil").innerHTML= "Kadar : "+hsl.toFixed(2);
     document.getElementById("w1").value="";
     document.getElementById("w2").value="";
