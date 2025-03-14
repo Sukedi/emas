04b78cbba70af392114a8c639b38aa3048356042
Sukedi <169124018+Sukedi@users.noreply.github.com>
2025-03-14 10:49:56 +0700

    Add files via upload

--- user.js
+++ user.js
@@ -5,7 +5,7 @@ async function device_id(nn){
     const screenHeight = window.screen.height;
     const fingerprint = `${userAgent};${platform};${screenWidth}x${screenHeight}`;
     shaJS = CryptoJS.SHA256(fingerprint).toString(CryptoJS.enc.Hex);
-    cekExist(shaJS,nn);
+    cekID(shaJS,nn);
 }
 
 async function cekExist(newNumber,nx) {
@@ -35,4 +35,33 @@ if (isExist) {
     if (nx==1){ shwPesan(); }
 }
 document.getElementById("usr").innerHTML= usrx;
+}
+
+async function cekID(uid,nx){
+    const url = "user.txt";
+    let usrx="[Username tak terdaftar]";
+    try {
+       const response = await fetch(url);
+       const text = await response.text();
+       const lines = text.split("\n").map(line => line.split(", ").map(item => item.trim()));
+       let ketemu = 0;
+       lines.forEach((row, i) => {
+          xrow = row;
+          row.forEach((col, j) => {
+              ret = `${col}`;
+              if (ret == uid){
+                 ketemu = 1;
+                 xrow.forEach((xx, n) => {
+              //id      if (n==0){ id = `${xx}: `; }
+                    if (n==1){ usrx = `${xx}, `; }
+              //tgl      if (n==2){ tgl = `${xx}.`; }
+               });
+          }
+       });
+    });
+    if (ketemu == 1){
+        if (nx==1){ shwPesan(); }
+    }
+    } catch (error) { }
+    document.getElementById("usr").innerHTML= usrx;
 }
